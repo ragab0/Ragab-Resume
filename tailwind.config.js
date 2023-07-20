54e4c498b7a5bb22b2dcd1acffb3723823a756c3
ragab <legendragab@gmail.com>
2023-07-20 16:28:29 +0300

    V2 main update. Converting to tailwindcss, Updating html structures for Good SEO

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./build/index.html"],
+  theme: {
+    extend: {
+      colors: {
+        mainDark: "#1e2532",
+        mainLight: "rgb(253 253 253)",
+      }
+    },
+  },
+  plugins: [],
+}
